b67cd2fd5f8b48469b98a8bc70d4bf4439afc579
TitanProgrammer4480 <sebi-2007@web.de>
2025-02-27 14:49:39 +0100

    added sites

---
+++ actions/auth.js
@@ -0,0 +1,19 @@
+"use server"
+
+import { RegisterFormSchema } from "@/lib/rules";
+
+export async function register(sate, formData) {
+
+    const validateFields = RegisterFormSchema.safeParse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+        confirmPassword: formData.get("confirmPassword"),
+    });
+
+    if(!validateFields.success) {
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+        };
+    }
+    console.log(validateFields);
+}
---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+            <nav>
+            <Link className="nav-link" href="/">Home</Link>
+            <div>
+              <Link className="nav-link" href="/register">Register</Link>
+              <Link className="nav-link" href="/dashboard">Dashboard</Link>
+            </div>
+          </nav>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          footer
+        </footer>
+      </body>
+    </html>
+  );
+}
